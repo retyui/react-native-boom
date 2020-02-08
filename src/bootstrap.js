2d357917859dfff5c4beaae937066b0b503b1dec
David <4661784+retyui@users.noreply.github.com>
2020-02-08 14:58:49 +0300

    Updates

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,5 +3,5 @@ import "./features/notifications/createDefaultChannel";
 if (process.env.NODE_ENV !== "production") {
   const { YellowBox } = require("react-native");
 
-  YellowBox.ignoreWarnings(["Require cycle:"]);
+  YellowBox.ignoreWarnings(["Require cycle:", "componentWillReceiveProps"]);
 }
